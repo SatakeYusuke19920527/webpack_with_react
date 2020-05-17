8713725e3b97e7e111297118c6993e35e0dababc
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-05-18 08:14:43 +0900

    file-loader url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,15 @@ module.exports = {
 			{
 				test: /\.css$/,
 				use: [ 'style-loader', 'css-loader' ]
-			}
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|ico)$/i,
+				loader: 'url-loader',
+				options: {
+					limit: 2048,
+					name:'./images/[name].[ext]'
+				}
+			},
 		]
 	},
 	devServer: {
